fe65dc01228ce3ec311e81ef7c933ddf510d23ea
ddanielos <dany.oblitas1984@gmail.com>
2020-11-17 17:57:43 -0500

    day3

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ListOfCategories } from './components/ListOfCategries'
+import { GlobalStyle } from './GlobalStyles'
+
+export const App = () => (
+  <>
+    <GlobalStyle/>
+    <ListOfCategories />
+  </>
+)
